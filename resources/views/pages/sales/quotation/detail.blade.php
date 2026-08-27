a7e8df90e8da76b65a49c0f9d4f2de6d8688a72f
devreftech <dev.reftech@gmail.com>
2026-08-27 15:40:02 +0700

    App Reftech V 2.3.2
    FIX :
    - PPN & Non-PPN

--- resources/views/pages/sales/quotation/detail.blade.php
+++ resources/views/pages/sales/quotation/detail.blade.php
@@ -516,6 +516,7 @@
                                     @else
                                         <button type="button" class="btn btn-whatsapp d-grid w-100 waves-effect mb-3 btn-upload-po"
                                             data-npwp="{{ $quote->pic->client->npwp ?? '' }}"
+                                            data-tax="{{ $quote->tax }}"
                                             data-client-url="{{ $quote->pic->client->role == 'Leads' ? route('detail.leads', $quote->pic->client->id) : route('existing.show', $quote->pic->client->id) }}">Upload PO</button>
                                     @endif
                                 @endif
